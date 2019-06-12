8ddf6389dcf94ef4e363fe611511f007155b487c
ian.hsu <deva1215@gmail.com>
2019-06-12 14:56:06 +0800

    Add gtags-cscope parse function.

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import ContentProvider, { encodeLocation } from './provider';
+import ContentProvider, { encodeLocation, scopemode } from './provider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,22 +12,52 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 
 	const providerRegistrations = vscode.Disposable.from(
-		vscode.workspace.registerTextDocumentContentProvider(ContentProvider.scheme, provider),
+		vscode.workspace.registerTextDocumentContentProvider(
+			ContentProvider.scheme, provider),
 	);
-
-	const commandRegistration = vscode.commands.registerTextEditorCommand('editor.printReferences', editor => {
-		let select: string;
-		select = editor.document.getText(editor.document.getWordRangeAtPosition(editor.selection.active));
-		if ((select) && (editor.document.getText().length !== select.length)) {
-			const uri = encodeLocation(editor.document.uri, editor.selection.active, select);
-			return vscode.workspace.openTextDocument(uri).then(doc => vscode.window.showTextDocument(doc, editor.viewColumn! + 1));
-		}
-		return null;
-	});
-
+	const lookupSymbolRegistration = vscode.commands.registerTextEditorCommand(
+		'editor.LookupSymbol', editor => {
+			let select: string;
+			select = editor.document.getText(editor.document.getWordRangeAtPosition(
+				editor.selection.active));
+			if ((select) && (editor.document.getText().length !== select.length)) {
+				const uri = encodeLocation(editor.document.uri, editor.selection.active,
+					select, scopemode.FIND_SYMBOL);
+				return vscode.workspace.openTextDocument(uri).then(
+					doc => vscode.window.showTextDocument(doc, editor.viewColumn! + 1));
+			}
+			return null;
+		});
+	const lookupGlobalDefRegistration = vscode.commands.registerTextEditorCommand(
+		'editor.LookupGlobalDef', editor => {
+			let select: string;
+			select = editor.document.getText(editor.document.getWordRangeAtPosition(
+				editor.selection.active));
+			if ((select) && (editor.document.getText().length !== select.length)) {
+				const uri = encodeLocation(editor.document.uri, editor.selection.active,
+					select, scopemode.FIND_GLOBAL_DEFINITION);
+				return vscode.workspace.openTextDocument(uri).then(
+					doc => vscode.window.showTextDocument(doc, editor.viewColumn! + 1));
+			}
+			return null;
+		});
+	const findTextRegistration = vscode.commands.registerTextEditorCommand(
+		'editor.FindText', editor => {
+			let select: string;
+			select = editor.document.getText(editor.selection);
+			if (select) {
+				const uri = encodeLocation(editor.document.uri, editor.selection.active,
+					select, scopemode.FIND_THID_TEXT_STRING);
+				return vscode.workspace.openTextDocument(uri).then(
+					doc => vscode.window.showTextDocument(doc, editor.viewColumn! + 1));
+			}
+			return null;
+		});
 	context.subscriptions.push(
 		providerRegistrations,
-		commandRegistration,
+		lookupSymbolRegistration,
+		lookupGlobalDefRegistration,
+		findTextRegistration,
 		provider);
 }
 
--- src/provider.ts
+++ src/provider.ts
@@ -5,6 +5,12 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import { execSync } from 'child_process';
+
+export enum scopemode {
+  FIND_SYMBOL = 0,
+  FIND_GLOBAL_DEFINITION = 1,
+  FIND_THID_TEXT_STRING = 4,
+};
 export default class Provider implements vscode.TextDocumentContentProvider {
 
   static scheme = 'references';
@@ -17,33 +23,105 @@ export default class Provider implements vscode.TextDocumentContentProvider {
   dispose() {
   }
 
+  parseScopeInformation(scope: string): [string, string, string] {
+    let Info = scope.split(" ");
+    let Path: string = "";
+    let Line: string = "";
+    let Desc: string = "";
+    let dIdx: number = 0;
+    Path = Info[dIdx++];
+    dIdx++;
+    Line = Info[dIdx++];
+    while (dIdx < Info.length) {
+      Desc += Info[dIdx++];
+      Desc += " ";
+    }
+    return [Path, Line, Desc];
+  }
+
   // Provider method that takes an uri of the `references`-scheme and
   // resolves its content by (1) running the reference search command
   // and (2) formatting the results
   provideTextDocumentContent(uri: vscode.Uri): string | Thenable<string> {
-    const [target, pos, symbol] = decodeLocation(uri);
-    let result : string;
-    let cmd : string;
-    cmd = 'gtags-cscope -dL -0 ';
+    const [target, pos, symbol, mode] = decodeLocation(uri);
+    let result: string;
+    let cmd: string;
+    let parse: string = "";
+    let CurrentPath: string = "";
+    let dIdx: number = 0;
+    let scope;
+    cmd = 'gtags-cscope -dL ';
+    cmd += ("-" + mode.toString() + " ");
     cmd += symbol;
     try {
-      result = execSync(cmd, { encoding: "UTF-8", cwd: vscode.workspace.rootPath}).toString();
+      result = execSync(cmd, {
+        encoding: "UTF-8",
+        cwd: vscode.workspace.rootPath
+      }).toString();
     } catch (error) {
       vscode.window.showInformationMessage('Occurred error' + error);
       return "";
     }
-    return result;
+    scope = result.split("\n");
+    switch (mode) {
+      case scopemode.FIND_SYMBOL:
+        parse += '------------>Find Symbol(';
+        parse += symbol;
+        parse += ')\n\n\n';
+        break;
+      case scopemode.FIND_GLOBAL_DEFINITION:
+        parse += '------------>Find Global Definition(';
+        parse += symbol;
+        parse += ')\n\n\n';
+        break;
+      case scopemode.FIND_THID_TEXT_STRING:
+        parse += '------------>Find Thid Text String(';
+        parse += symbol;
+        parse += ')\n\n\n';
+        break;
+      default:
+        vscode.window.showInformationMessage('Error scope mode');
+        return "";
+        break;
+    }
+    while (dIdx < scope.length) {
+      const [path, line, desc] = this.parseScopeInformation(scope[dIdx]);
+      if ((path !== "") && (line !== "") && (desc !== "")) {
+        if (CurrentPath.match(path)) {
+          parse += '\t\t';
+          parse += line;
+          parse += ':  ';
+          parse += desc;
+          parse += '\n';
+        }
+        else {
+          CurrentPath = path;
+          parse += path;
+          parse += ':\n';
+          parse += '\t\t';
+          parse += line;
+          parse += ':  ';
+          parse += desc;
+          parse += '\n';
+        }
+      }
+      dIdx += 1;
+    }
+    return parse;
   }
 }
 
 let seq = 0;
 
-export function encodeLocation(uri: vscode.Uri, pos: vscode.Position, symbol: string): vscode.Uri {
-  const query = JSON.stringify([uri.toString(), pos.line, pos.character, symbol]);
+export function encodeLocation(uri: vscode.Uri, pos: vscode.Position,
+  symbol: string, Mode: scopemode): vscode.Uri {
+  const query = JSON.stringify([uri.toString(), pos.line, pos.character, symbol, Mode]);
   return vscode.Uri.parse(`${Provider.scheme}:References.locations?${query}#${seq++}`);
 }
 
-export function decodeLocation(uri: vscode.Uri): [vscode.Uri, vscode.Position, string] {
-  let [target, line, character, symbol] = <[string, number, number, string]>JSON.parse(uri.query);
-  return [vscode.Uri.parse(target), new vscode.Position(line, character), symbol];
+function decodeLocation(uri: vscode.Uri):
+  [vscode.Uri, vscode.Position, string, scopemode] {
+  let [target, line, character, symbol, mode] =
+    <[string, number, number, string, scopemode]>JSON.parse(uri.query);
+  return [vscode.Uri.parse(target), new vscode.Position(line, character), symbol, mode];
 }
